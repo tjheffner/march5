87322650ed1f22d8503da93bb1faa9957b1d82b6
Tanner Heffner & Liz Beacham <student@epicodus.com>
2015-03-05 11:29:40 -0800

    Add twig, moved car arrays from car.php to app.php

---
+++ car/app/app.php
@@ -0,0 +1,50 @@
+<?php
+    require_once __DIR__."/../vendor/autoload.php";
+    require_once __DIR__."/../src/Car.php";
+
+    session_start();
+    if (empty($_SESSION['list_of_cars'])) {
+        $_SESSION['list_of_cars'] = array();
+    }
+    //session info goes here
+
+    $app = new Silex\Application();
+
+    $app->register(new Silex\Provider\TwigServiceProvider(), array (
+        'twig.path' => __DIR__.'/../views'
+    ));
+    //twig goes here
+
+    $app->get("/", function() use ($app) {
+        return $app['twig']->render('cars.twig', array('cars' => Car::getAll()));
+    });
+
+    $app->post("/cars", function() use ($app) {
+        $car = new Car($_POST['model'], $_POST['price'], $_POST['miles']);
+        $car->save();
+
+        $first_car = new Car("2014 Porsche 911", 114991, 7864);
+        $second_car = new Car("2011 Ford F450", 55995, 14241);
+        $third_car = new Car("2013 Lexus RX 350", 44700, 20000);
+        $fourth_car = new Car("Mercedes Benz CLS550", 39900, 37979);
+
+        $cars = array($first_car, $second_car, $third_car, $fourth_car);
+
+        $list_of_cars = array();
+        foreach ($cars as $car) {
+            if ($car->worthBuying($_POST["price"])) {
+                array_push($list_of_cars, $car);
+            }
+        }
+
+
+        return $app['twig']->render('car_results.twig', array('list_of_cars' => $car));
+                
+    });
+
+    $app->post("/delete", function() use ($app) {
+        return $app['twig']->render('delete.twig', array('cars' => Car::deleteAll()));
+    });
+
+    return $app;
+?>
